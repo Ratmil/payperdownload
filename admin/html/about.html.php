e6cf31a942c96e482764c25cd3cd9d9f614b2766
ratmil <eclipse@mysimplenetwork.com>
2017-03-14 09:09:56 -0400

    Updated xml version number. Added thanks text to Olivier Buisard

--- admin/html/about.html.php
+++ admin/html/about.html.php
@@ -20,9 +20,11 @@ class AboutHtmlForm extends BaseHtmlForm
 		<div style="float:right;">
 		<img src="<?php echo $root;?>administrator/components/com_payperdownload/images/ppdplus.png" />
 		</div>
-		<p><font size="5"><strong>Payperdownload 5.6</strong></font> </p>
+		<p><font size="5"><strong>Payperdownload 5.7</strong></font> </p>
 		<p><font size="5"><strong>Copyright</strong></font></p>
 		<p><font size="4">&nbsp;&nbsp;&nbsp;©&nbsp;2010 - <?php echo date("Y"); ?> Ratmil&nbsp;&nbsp;&nbsp;<a href="http://www.ratmilwebsolutions.com">www.ratmilwebsolutions.com</a></font></p>
+		<p><font size="4"><strong>Bug Fixes</strong></font></p>
+		<p><font size="4">&nbsp;&nbsp;&nbsp;Olivier Buisard&nbsp;&nbsp;&nbsp;<a href="https://simplifyyourweb.com/">www.simplifyyourweb.com</a></font></p>
 		<p><font size="4"><a href="http://extensions.joomla.org/extensions/e-commerce/paid-downloads/18146"><?php echo JText::_("PAYPERDOWNLOADPLUS_WRITE_REVIEW");?></a></font></p>
 		<p><font size="5"><strong>License.</strong></font></p>
 		<font size="4"><a href="http://www.gnu.org/licenses/lgpl-3.0.html">Gnu Public License</a></font>
